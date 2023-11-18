6b8a0ebf915b27ee59c08312e3532712d838a35b
edusantanaw <eduardosantana20122002@gmail.com>
2023-11-17 22:39:45 -0300

    feat: buildRequestObj

--- src/router/router-executer.ts
+++ src/router/router-executer.ts
@@ -1,6 +1,24 @@
 import http from "http";
 
-export function routerExecuter() {}
+export function routerExecuter(
+  req: http.IncomingMessage,
+  res: http.ServerResponse<http.IncomingMessage> & {
+    req: http.IncomingMessage;
+  }
+) {}
+
+export async function buildRequestObj(req: http.IncomingMessage) {
+  const method = req.method;
+  const body = await getBody(req);
+  const url = req.url ?? "";
+  const query = getQuery(url);
+  return {
+    ...req,
+    method,
+    body,
+    query,
+  };
+}
 
 function getBody(req: http.IncomingMessage) {
   let data = "";
@@ -11,5 +29,15 @@ function getBody(req: http.IncomingMessage) {
     req.on("end", () => {
       resolve(JSON.parse(data));
     });
+  });
+}
+
+function getQuery(url: string) {
+  const [_, ...rest] = url?.split("?");
+  const queryString = rest.reduce((acc, item) => (acc += item), "");
+  const query: any = {};
+  queryString.split("&").forEach((v) => {
+    let [key, value] = v.split("=");
+    query[key] = value;
   });
 }
